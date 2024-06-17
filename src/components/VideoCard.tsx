0efef75084ab2f76af559305f3a817e782dd4857
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-17 23:12:54 +0530

    new changes

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,8 +1,13 @@
 
-
+//48mins video 8.0
 const VideoCard = () => {
   return (
-    <div>VideoCard</div>
+    <div>
+    <img src="../public/thumbnail.jpg" alt="" />
+    <div className="grid grid-cols-12"></div>
+    <div className="grid-span-2"></div>
+    <img src="../public/thumbnail.jpg" alt="" />
+    </div>
   )
 }
 
